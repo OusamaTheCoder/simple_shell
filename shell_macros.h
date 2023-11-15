592639fdd960c6ed04282c1ff4213a1f211e5429
Ousama Oujaber <ndnd15237@gmail.com>
2023-11-14 17:19:13 -0800

    0x16. C - Simple Shell

--- shell_macros.h
+++ shell_macros.h
@@ -15,7 +15,8 @@
 #define HELP_CD_MSG "cd=\n"\
 "cd:\tcd [dir]\n\n"\
 "	Alter the current working directory of the shell.\n\n"\
-"	If the cd command is used without providing any arguments, it will be interpreted as follows:\n"\
+"	If the cd command is used without providing any arguments, \n"\
+"	it will be interpreted as follows:\n"\
 "	as cd $HOME.\n"\
 "	If the argument is - (cd -), the command will be interpreted:\n"\
 "	as cd $OLDPWD.\n\n"
@@ -23,7 +24,8 @@
 #define HELP_EXIT_MSG "exit=\n"\
 "exit:\texit [STATUS]\n\n"\
 "	Exit of the simple_shell.\n\n"\
-"	Quits the shell with an exit status of N. If N is not provided, the exit status\n"\
+"	Quits the shell with an exit status of N.\n"\
+"	If N is not provided, the exit status\n"\
 "	will be that of the last executed command.\n\n"
 
 #define HELP_ENV_MSG "env=\n"\
@@ -45,7 +47,9 @@
 #define HELP_MSG "help=\n"\
 "help:\thelp [BUILTIN_NAME]\n\n"\
 "	Display information about builtin commands.\n\n"\
-"	Summary of builtin commands. If BUILTIN_NAME is specified, provides detailed help on commands matching BUILTIN_NAME.\n"\
+"	Summary of builtin commands.\n"\
+"	If BUILTIN_NAME is specified,\n"\
+"	provides detailed help on commands matching BUILTIN_NAME.\n"\
 "   If not, the list of help topics is printed.\n"\
 "	Arguments:\n\n"\
 "	BUILTIN_NAME specifying a help topic.\n\n"\
